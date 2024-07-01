f434f65d8882a10951bfb02052cad73e348fd8ab
Tica506 <slentz3@wgu.edu>
2024-07-01 16:23:28 -0400

    initial commit

---
+++ src/main/java/com/example/lab_java_intro_to_jpa/LabJavaIntroToJpaApplication.java
@@ -0,0 +1,13 @@
+package com.example.lab_java_intro_to_jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabJavaIntroToJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabJavaIntroToJpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/lab_java_intro_to_jpa/LabJavaIntroToJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lab_java_intro_to_jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabJavaIntroToJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
